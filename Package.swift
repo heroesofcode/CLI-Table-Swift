f437011706d70b2a83d2988f9f90d7009a7cc57d
joaolfp <joaolucasfp2001@gmail.com>
2024-06-20 18:57:27 -0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CLITable",
+    products: [
+        .library(
+            name: "CLITable",
+            targets: ["CLITable"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "CLITable"),
+        .testTarget(
+            name: "CLITableTests",
+            dependencies: ["CLITable"]
+        ),
+    ]
+)
